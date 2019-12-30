0cffac8512fe7a6e02fa8e5e39a9d15dca1e3318
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-12-30 16:06:21 +0200

    convert this code in Lambda

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                String myString = "Let's split this up into an array";
+                String[] parts = myString.split(" ");
+                for (String part : parts) {
+                    System.out.println(part);
+                }
+            }
+        };
+
+    }
+}
