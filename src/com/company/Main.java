2d4c40eb5fbe6401fb5ce5bb30d45d262fd1f678
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-12-30 16:18:19 +0200

    create lambda with this code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -22,5 +22,15 @@ public class Main {
             }
         };
 
+        (String s) -> {
+            StringBuilder returnVal = new StringBuilder();
+            for (int i = 0; i < s.length(); i++) {
+                if (i % 2 == 1) {
+                    returnVal.append(s.charAt(i));
+                }
+            }
+            return returnVal.toString();
+        };
+
     }
 }
