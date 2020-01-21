2202b208061d57c21ee9a87c97347948d0ff7b2f
Serhii Miroshnichenko <ragazzo@ukr.net>
2020-01-21 15:21:50 +0200

    a little update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -61,6 +62,12 @@ public class Main {
 //        firstUpperCaseList.forEach(System.out::println);
 
         topNames2015.stream().map(name -> name.substring(0, 1).toUpperCase() + name.substring(1)).sorted(String::compareTo).forEach(System.out::println);
+
+
+        topNames2015.stream().map(name -> name.substring(0, 1).toUpperCase() + name.substring(1))
+                .peek(System.out::println)
+                .sorted(String::compareTo)
+                .collect(Collectors.toList());
 
 //        long namesBeginingWithA = topNames2015.stream().map(name -> name.substring(0, 1).toUpperCase() + name.substring(1)).filter(name -> name.startsWith("A")).count();
 //
