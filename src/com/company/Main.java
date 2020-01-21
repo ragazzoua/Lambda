487ad3d69643b7d3625ee7066d8fb3a0d15d56b6
Serhii Miroshnichenko <ragazzo@ukr.net>
2020-01-21 11:35:31 +0200

    a little update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -55,8 +55,10 @@ public class Main {
         List<String> firstUpperCaseList = new ArrayList<>();
 
         topNames2015.forEach(name -> firstUpperCaseList.add(name.substring(0,1).toUpperCase() + name.substring(1)));
-        firstUpperCaseList.sort((s1, s2) -> s1.compareTo(s2));
-        firstUpperCaseList.forEach(s -> System.out.println(s));
+//        firstUpperCaseList.sort((s1, s2) -> s1.compareTo(s2));
+//        firstUpperCaseList.forEach(s -> System.out.println(s));
+        firstUpperCaseList.sort(String::compareTo);
+        firstUpperCaseList.forEach(System.out::println);
 
 
     }
